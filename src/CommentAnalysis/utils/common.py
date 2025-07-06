94edfa687edc254b036b555c27b18258f3ad77f8
AIwithAj <dancerworld60@gmail.com>
2025-07-06 10:14:11 -0700

    dataIngestion stage addedd

--- src/CommentAnalysis/utils/common.py
+++ src/CommentAnalysis/utils/common.py
@@ -1,125 +1,129 @@
 import os
-from box.exceptions import BoxValueError
-import yaml
-from src.CommentAnalysis import logger
 import json
+import yaml
 import joblib
-from ensure import ensure_annotations
 from box import ConfigBox
+from box.exceptions import BoxValueError
 from pathlib import Path
-from typing import Any
+from typing import Any, Iterable, Union
+from typeguard import typechecked
+from src.CommentAnalysis import logger
 
 
-
-@ensure_annotations
-def read_yaml(path_to_yaml: Path) -> ConfigBox:
-    """reads yaml file and returns
+@typechecked
+def read_yaml(path_to_yaml: Union[str, Path]) -> ConfigBox:
+    """
+    Read a YAML file and return its contents as a ConfigBox.
 
     Args:
-        path_to_yaml (str): path like input
+        path_to_yaml (str | Path): Path to the YAML file.
 
     Raises:
-        ValueError: if yaml file is empty
-        e: empty file
+        ValueError: If YAML file is empty.
+        Exception: For any other error.
 
     Returns:
-        ConfigBox: ConfigBox type
+        ConfigBox: Parsed YAML content.
     """
     try:
-        with open(path_to_yaml) as yaml_file:
+        with open(path_to_yaml, "r", encoding="utf-8") as yaml_file:
             content = yaml.safe_load(yaml_file)
-            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            if content is None:
+                raise ValueError("YAML file is empty")
+            logger.info(f"YAML file loaded successfully: {path_to_yaml}")
             return ConfigBox(content)
-    except BoxValueError:
-        raise ValueError("yaml file is empty")
+    except BoxValueError as e:
+        raise ValueError("Invalid YAML content") from e
     except Exception as e:
-        raise e
-    
+        raise RuntimeError(f"Failed to read YAML file: {path_to_yaml}") from e
 
 
-@ensure_annotations
-def create_directories(path_to_directories: list, verbose=True):
-    """create list of directories
+@typechecked
+def create_directories(paths: Iterable[Union[str, Path]], verbose: bool = True) -> None:
+    """
+    Create directories if they don't exist.
 
     Args:
-        path_to_directories (list): list of path of directories
-        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+        paths (Iterable[str | Path]): List or set of directories to create.
+        verbose (bool, optional): Log the creation. Defaults to True.
     """
-    for path in path_to_directories:
+    for path in paths:
         os.makedirs(path, exist_ok=True)
         if verbose:
-            logger.info(f"created directory at: {path}")
+            logger.info(f"Created directory: {path}")
 
 
-@ensure_annotations
-def save_json(path: Path, data: dict):
-    """save json data
-
-    Args:
-        path (Path): path to json file
-        data (dict): data to be saved in json file
+@typechecked
+def save_json(path: Union[str, Path], data: dict) -> None:
     """
-    with open(path, "w") as f:
-        json.dump(data, f, indent=4)
-
-    logger.info(f"json file saved at: {path}")
-
-
-
-
-@ensure_annotations
-def load_json(path: Path) -> ConfigBox:
-    """load json files data
+    Save data to a JSON file.
 
     Args:
-        path (Path): path to json file
+        path (str | Path): Path to save the JSON file.
+        data (dict): Data to save.
+    """
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+    logger.info(f"JSON file saved: {path}")
+
+
+@typechecked
+def load_json(path: Union[str, Path]) -> ConfigBox:
+    """
+    Load data from a JSON file.
+
+    Args:
+        path (str | Path): Path to the JSON file.
 
     Returns:
-        ConfigBox: data as class attributes instead of dict
+        ConfigBox: Loaded data.
     """
-    with open(path) as f:
+    with open(path, "r", encoding="utf-8") as f:
         content = json.load(f)
-
-    logger.info(f"json file loaded succesfully from: {path}")
+    logger.info(f"JSON file loaded: {path}")
     return ConfigBox(content)
 
 
-@ensure_annotations
-def save_bin(data: Any, path: Path):
-    """save binary file
+@typechecked
+def save_bin(data: Any, path: Union[str, Path]) -> None:
+    """
+    Save an object to a binary file using joblib.
 
     Args:
-        data (Any): data to be saved as binary
-        path (Path): path to binary file
+        data (Any): Data to save.
+        path (str | Path): Path to save the binary file.
     """
     joblib.dump(value=data, filename=path)
-    logger.info(f"binary file saved at: {path}")
+    logger.info(f"Binary file saved: {path}")
 
 
-@ensure_annotations
-def load_bin(path: Path) -> Any:
-    """load binary data
+@typechecked
+def load_bin(path: Union[str, Path]) -> Any:
+    """
+    Load an object from a binary file.
 
     Args:
-        path (Path): path to binary file
+        path (str | Path): Path to the binary file.
 
     Returns:
-        Any: object stored in the file
+        Any: Loaded object.
     """
     data = joblib.load(path)
-    logger.info(f"binary file loaded from: {path}")
+    logger.info(f"Binary file loaded: {path}")
     return data
 
-@ensure_annotations
-def get_size(path: Path) -> str:
-    """get size in KB
+
+@typechecked
+def get_size(path: Union[str, Path]) -> str:
+    """
+    Get size of file in KB.
 
     Args:
-        path (Path): path of the file
+        path (str | Path): Path to the file.
 
     Returns:
-        str: size in KB
+        str: Size as a string.
     """
-    size_in_kb = round(os.path.getsize(path)/1024)
-    return f"~ {size_in_kb} KB"
-
+    size_in_kb = round(os.path.getsize(path) / 1024)
+    logger.info(f"Size of {path}: ~{size_in_kb} KB")
+    return f"~{size_in_kb} KB"
